6b0469f26990e8d43c37f9e96c5c92ef355cf6a9
b10t <b10t.rus@gmail.com>
2022-02-24 08:22:51 +0800

    Move code to reusable QuerySet method.

--- blog/views.py
+++ blog/views.py
@@ -115,10 +115,8 @@ def post_detail(request, slug):
 
 def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
-    # related_posts = tag.posts.all()[:20].annotate(
-    #     num_comments=Count('comments'))
-
-    related_posts = tag.posts.all()[:20]
+    related_posts = tag.posts.annotate(
+        num_comments=Count('comments'))[:20]
 
     most_popular_tags = Tag.objects.popular()[:5]
 
@@ -127,7 +125,7 @@ def tag_filter(request, tag_title):
     context = {
         'tag': tag.title,
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
-        'posts': [serialize_post(post) for post in related_posts],
+        'posts': [serialize_post_optimized(post) for post in related_posts],
         'most_popular_posts': [
             serialize_post_optimized(post) for post in most_popular_posts
         ],
