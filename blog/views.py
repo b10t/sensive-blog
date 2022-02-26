13091709b458c503eb5039dafe31a625a8295e73
b10t <b10t.rus@gmail.com>
2022-02-26 08:53:29 +0800

    Secured the receipt of the post.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.db.models import Count, Prefetch
+from django.db.models import Count
+from django.http import Http404
 from django.shortcuts import render
 
 from blog.models import Comment, Post, Tag
@@ -50,7 +51,11 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.get(slug=slug)
+    try:
+        post = Post.objects.get(slug=slug)
+    except Post.DoesNotExist:
+        raise Http404('Post does not exist')
+
     comments = Comment.objects.filter(post=post).prefetch_related('author')
     serialized_comments = []
     for comment in comments:
